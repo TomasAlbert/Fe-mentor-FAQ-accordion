7143363633b28d67a3b30322869cf5b451b4ebe9
TomasAlbert <tomasalbert9387182@gmail.com>
2023-12-05 14:08:05 +0100

    chore: setup tailwind config add colors,font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
 	content: ["./**/*.{html,js}"],
 	theme: {
-		extend: {},
+		screens: {
+			sm: "375px",
+			md: "768px",
+			lg: "976px",
+			xl: "1440px",
+		},
+		colors: {
+			white: "hsl(0, 0%, 100%)",
+			lightPink: "hsl(275, 100%, 97%)",
+			GrayishPurple: "hsl(292, 16%, 49%)",
+			DarkPurple: "hsl(292, 42%, 14%)",
+		},
+		fontFamily: {
+			sans: ["Work Sans", "sans-serif"],
+		},
 	},
 	plugins: [],
 };
